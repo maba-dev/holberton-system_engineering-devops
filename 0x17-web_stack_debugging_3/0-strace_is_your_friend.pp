1b5e3c48241df028ebed2abc32aedc38f26934e4
maba-dev <cherif.ba@hotmail.fr>
2021-09-29 15:11:57 +0200

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,3 @@
+exec { '/var/www/html/wp-setting.php':
+path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
