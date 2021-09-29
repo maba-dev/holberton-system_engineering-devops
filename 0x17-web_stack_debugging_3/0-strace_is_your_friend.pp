364aa5f0613b299a0791e7f5ba3fedda73bafad8
maba-dev <cherif.ba@hotmail.fr>
2021-09-29 15:14:26 +0200

    task 0 retry

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+#debugging
 exec { '/var/www/html/wp-setting.php':
 path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
 command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
