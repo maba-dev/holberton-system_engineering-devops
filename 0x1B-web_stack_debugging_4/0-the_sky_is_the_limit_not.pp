05a29028221fdc8f18fb5247432f40ef502fccb0
maba-dev <cherif.ba@hotmail.fr>
2021-10-07 15:22:04 +0200

    retry

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Sky is the limit, let's bring that limit higher
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/2000/" etc/default/nginx',
+  command => 'sed -i "s/15/20000/" etc/default/nginx',
   path    => '/bin/:/usr/local/bin/',
 }
 
