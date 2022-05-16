3ff91e2cf5d6ff1546f953d970ee1f11d4a6ed6f
maba-dev <cherif.ba@hotmail.fr>
2022-05-16 20:33:56 +0000

    correct pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,9 @@ import requests
 from sys import argv
 
 
-if __name__== "__main__":
-    api_url = ('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+if __name__ == "__main__":
+    api_url = ('https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        argv[1]))
     name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                         .format(argv[1])).json()['name']
 
@@ -13,13 +14,12 @@ if __name__== "__main__":
     number_of_completed_tasks = 0
     total_number_of_asks = 0
     title_tab = []
-    
     for i in response.json():
         total_number_of_asks += 1
         if i['completed']:
             number_of_completed_tasks += 1
             title_tab.append(i['title'])
-    
-    print("Employee {:s} is done with tasks({:d}/{:d}):".format(name, number_of_completed_tasks, total_number_of_asks))
+    print("Employee {:s} is done with tasks({:d}/{:d}):".format(
+        name, number_of_completed_tasks, total_number_of_asks))
     for j in title_tab:
         print("\t {}".format(j))
