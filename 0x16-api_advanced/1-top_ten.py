f0aeb27a4edc2164f531861106130154aa73ef85
maba-dev <cherif.ba@hotmail.fr>
2022-05-31 20:27:14 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a function that queries the API and prints the titles of the first 10 hot"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    subs = requests.get(
+        'https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+        headers={'User-Agent': 'cherif'}
+    )
+    if subs.status_code == 404:
+        print("None")
+    subs = subs.json()
+    data = subs.get('data').get('children')
+    for i in data[:10]:
+        print(i['data']['title'])
