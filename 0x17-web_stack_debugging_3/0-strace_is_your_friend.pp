39042410e81e1fce46eac16c3f65b9c45ddf1f09
maba-dev <cherif.ba@hotmail.fr>
2022-06-02 12:39:21 +0000

    correct the bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
 #Regex to change the word phpp in php
-
-
-exec { ''Change of word':
-command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+exec { 'Change of word':
+command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 provider => shell
 }
