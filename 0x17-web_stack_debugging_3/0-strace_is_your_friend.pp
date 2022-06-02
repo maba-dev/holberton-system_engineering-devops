513386cb9a100c2adeadcabf50a21e7d0facefca
maba-dev <cherif.ba@hotmail.fr>
2022-06-02 12:36:51 +0000

    correct the bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 #Regex to change the word phpp in php
 
 
-exec { 'Change of word':
-  path    => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  provider    => shell,
+exec { ''Change of word':
+command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+provider => shell
 }
